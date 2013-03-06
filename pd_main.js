55bb32f7c77b94daf1ae476111f806293ff3dea1
Wilson Lin <xjmtpomjo@gmail.com>
2013-03-06 23:30:31 +0800

    yet another play-around.
    Signed-off-by: Wilson Lin <xjmtpomjo@gmail.com>

--- pd_main.js
+++ pd_main.js
@@ -1,38 +1,46 @@
-// A generic onclick callback function.
-function genericOnClick(info, tab) {
-  // console.log("item " + info.menuItemId + " was clicked");
-  // console.log("info: " + JSON.stringify(info));
-  // console.log("tab: " + JSON.stringify(tab));
-  //alert(info.selectionText);
-  showNotifyDiv(info.selectionText);
+function toSaveWord(word)
+{
+  if (ntf) {
+     ntf.cancel();
+     clearTimeout(toForNTF);
+  }
+  showNotifyDiv(word.selectionText);
 }
 
-function showNotifyDiv(selTxt)
+function showNotifyDiv(selTxt, tab)
 {
-  var aDiv = document.createElement('div');
-  aDiv.style.opacity = '0.7';
-  aDiv.style.padding = '5px';
-  aDiv.style.fontSize = '10px';
-  aDiv.style.margin = '10px';
-  aDiv.style.position = 'absolute';
-  aDiv.style.minHeight = '50px';
-  aDiv.style.width = '250px';
-  aDiv.style.backgroundColor = '#badaaa';
-  aDiv.style.borderRadius = '10px';
-  aDiv.text = "\"" + selTxt + "\" is saved to your Dic.";
-  document.body.appendChild(aDiv);
+  //var aDiv = document.createElement('div');
+  //aDiv.style.opacity = '0.7';
+  //aDiv.style.padding = '5px';
+  //aDiv.style.fontSize = '10px';
+  //aDiv.style.margin = '10px';
+  //aDiv.style.position = 'absolute';
+  //aDiv.style.minHeight = '50px';
+  //aDiv.style.width = '250px';
+  //aDiv.style.backgroundColor = '#badaaa';
+  //aDiv.style.borderRadius = '10px';
+  //aDiv.style.display = 'block';
+  //aDiv.innerHTML = "\"" + selTxt + "\" is saved to your Dic.";
+  //aDiv.setAttribute('id','1aqsr3e4dasdasd');
+  //var bd = document.body;
+  //bd.insertBefore(aDiv, bd.childNodes[0]);
+    //alert(selTxt);
+    ntf = webkitNotifications.createNotification("dict.png", "\"" + selTxt + "\" is saved to your Dic.", "");
+    ntf.show();
+    toForNTF = setTimeout(function () {
+        ntf.cancel()
+    }, 3000);
 }
 
 // Create one test item for each context type.
-var contexts = ["page","selection","link","editable","image","video",
-                 "audio"];
-var id = chrome.contextMenus.create({"title": "Save this word to my dic.(?)", "contexts": contexts,
-                                       "onclick": genericOnClick});
-
+var pageId = chrome.contextMenus.create({"title": "Open Project Dic. Dashboard", "contexts": ["page"],
+                                       "onclick": null});
+var selectionId = chrome.contextMenus.create({"title": "Save this word to my dic.(?)", "contexts": ["selection"],
+                                       "onclick": toSaveWord});
+var ntf;
+var toForNTF;
 // Create a parent item and two children.
-var parent = chrome.contextMenus.create({"title": "Open Project Dic. Dashboard"});
-var child1 = chrome.contextMenus.create(
-  {"title": "Child 1", "parentId": parent, "onclick": genericOnClick});
-// var child2 = chrome.contextMenus.create(
-//   {"title": "Child 2", "parentId": parent, "onclick": genericOnClick});
+// var parent = chrome.contextMenus.create({"title": "Open Project Dic. Dashboard"});
+// var child1 = chrome.contextMenus.create(
+//   {"title": "Child 1", "parentId": parent, "onclick": toSaveWord});
 
